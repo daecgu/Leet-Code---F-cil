1c871722989a88177f5220aa76c6984ff2da8991
Daniel Echeverri <decheverri@helphone.com>
2023-11-03 08:55:43 +0100

    119 Pascal Triangle II.

--- 119 Pascal Triangle II.py
+++ 119 Pascal Triangle II.py
@@ -1,3 +1,4 @@
+# esta es la forma que se me ha ocurrido
 def get_row(row_index) -> list[int]:
     ans = [1]
     if row_index == 0:
@@ -19,3 +20,16 @@ def get_row(row_index) -> list[int]:
     return ans
 
 print(get_row(6))
+
+
+## esta es la forma matemática de calcular cada una de las posiciones
+def get_row_optimized(rowIndex):
+    res = [1]
+    prev = 1
+    for k in range (1, rowIndex +1):
+        next_val= prev * (rowIndex -k +1) // k
+        res.append(next_val)
+        prev = next_val
+    return res
+
+print(get_row_optimized(6))
